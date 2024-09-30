6a72a8fd29652b7f573530645ff51adcf5a5038e
github37-hub <robertnicdao4@gmail.com>
2024-09-30 01:12:22 -0400

    added r.js-quizapp

---
+++ 5gsr.js-quizapp/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuizApp from './Components/QuizApp/QuizApp'
+
+const App = () => {
+  return (
+    <div>
+      <QuizApp/>
+    </div>
+  )
+}
+
+export default App
